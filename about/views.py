b1a2de7f5f093ea19e2e7c50812647f139852510
minWachya <alal7272@gmail.com>
2021-11-26 16:56:20 +0900

    내가 작성한 댓글 보여주기

--- about/views.py
+++ about/views.py
@@ -4,7 +4,13 @@ from django.db.models import Q
 
 
 def about_me(request):
-    return render(request, 'about/me.html')
+    comment_list = Comment.objects.filter(author__username=request.user.username)
+
+    return render(request, 'about/me.html',
+                    {
+                        'comment_list':comment_list,
+                    },
+                  )
 
 
 # 회사 소개
